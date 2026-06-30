bbe0068a351a0d5f082cf42bcf9fb772f862de2b
Joseph Muñoz Santaella <josep.munozs@duocuc.cl>
2026-06-30 18:13:38 -0400

    Update StaticPageGenerator.java

--- src/main/java/com/eval3/frontend/StaticPageGenerator.java
+++ src/main/java/com/eval3/frontend/StaticPageGenerator.java
@@ -68,7 +68,7 @@ public class StaticPageGenerator {
 <body>
     <div class="container">
         <header>
-            <h1>Gestión de los Productos</h1>
+            <h1>Gestión productos</h1>
             <nav>
                 <button class="nav-btn active" onclick="showSection('products')">Productos</button>
                 <button class="nav-btn" onclick="showSection('register')">Registro</button>
